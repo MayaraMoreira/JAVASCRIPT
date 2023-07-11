fc0d5d20f334ef794aa43801a12c5e026256c7fd
MayaraMoreira <mayaraabragaa@gmail.com>
2023-07-11 17:47:50 -0300

    Aprendendo a usar o JS

---
+++ 1.variaveis.js
@@ -0,0 +1,19 @@
+/* 
+No JS temos 3 formas de criar as variáveis
+
+- let -> Posso colocar qualquer nome na variável? Não, elas não podem começar com numeros. Tem que ser uma palavra só, NÃO pode ter espaço!
+    Ex: duasLetras. Começar com letra minuscula, não importa o tamanho da frase. O QUE ESTÁ DEPOIS DO = É O QUE VC ESTA GUARDANDO DENTRO 
+    DA VARIÁVEL/ Só ao criar a variável eu preciso colocar "let", se eu for mudar o valor, ou a escrita dela depois, é só escrever o nome
+    dela = e o valor ou a escrita nova.
+
+- const -> Valor NÃO pode ser declarado NOVAMENTE. (O "lef" pode, essa é a diferença entre eles.)
+
+- var -> DESCONTINUADO. ELE É ANTIGO/ NÃO USAR.
+
+Usar sempre o const. Caso notar que o numero precisa ser alterado, usar let.
+*/
+
+const numeroDeAlunosNaSalaSete = 40
+
+
+console.log(numeroDeAlunosNaSalaSete)
